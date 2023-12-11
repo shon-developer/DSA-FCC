e5e9188ae67d6e226e1cb62fe3494cf46b5558df
shon <shonbwd@gmail.com>
2023-12-11 19:50:16 +0530

    first commit

---
+++ 01_collectionOfData.js
@@ -0,0 +1,5 @@
+//* Use an Array to Store a Collection of Data
+
+//? We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+
+let yourArray = ["Shon", 4, true, null, undefined];
---
+++ 02_bracketNotation.js
@@ -0,0 +1,6 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "b is no more in the array";
+
+// Only change code above this line
+console.log(myArray);
