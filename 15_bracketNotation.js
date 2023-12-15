42136ca2e9013c15a10f2923d97dc6ecbc678745
shon <shonbwd@gmail.com>
2023-12-15 12:13:05 +0530

    completed more tasks

---
+++ 15_bracketNotation.js
@@ -0,0 +1,21 @@
+//* Access Property Names with Bracket Notation
+
+//? We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  let selectedFood = foods[scannedItem];
+  return selectedFood;
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
